61ebb2a4f270b31db4f4125501745fe3755b21f0
paulo-rangel <paulorangel11n@gmail.com>
2026-01-18 18:35:18 -0300

    First Commit: adds folder dedicated to solutions using JS and solution for solveMeFirst exercise.

---
+++ JavaScript/solveMeFirst.js
@@ -0,0 +1,10 @@
+/*
+name: solveMeFirst
+date: 01-18-2026
+platform: hackerrank
+link: https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+*/
+
+function solveMeFirst(a, b) {
+  return a + b
+}

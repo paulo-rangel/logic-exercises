0a6669ff4ccb3482ef3b75ab32ddebcfef6250d3
paulo-rangel <paulorangel11n@gmail.com>
2026-02-07 16:51:48 -0300

    Adds solution in `aVeryBigSum.js`

---
+++ JavaScript/aVeryBigSum.js
@@ -0,0 +1,14 @@
+/*
+name: compare the triplets
+date: 02-07-2026
+platform: hackerrank
+link: https://www.hackerrank.com/challenges/a-very-big-sum/problem
+*/
+
+function aVeryBigSum(ar) {
+  let long = 0n;
+  ar.forEach((x) => {
+    long += BigInt(x);
+  });
+  return long;
+}
